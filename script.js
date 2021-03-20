15bddfac659ce678e4843b0e96bdac1f5aecce26
Aftaab Dobani <1234dobani@gmail.com>
2021-03-19 20:52:14 -0400

    added 4 sets of variables

--- script.js
+++ script.js
@@ -2,12 +2,15 @@
 // 1. When the button is pressed a prompt appears (done)
 // 2. User then selects pw criteria 
 // 3. Length of password has to be at least 8 or less than 128 characters
-// 4. Inclues lowercase, uppercase, numeric, & special characters 
+// 4. Includes lowercase, uppercase, numeric, & special characters 
 // 5. After password is generated, it needs to appear on the page (alert message)
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var characters =  "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+var specialCharacters = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~ "
+var uppercase = " ABCDEFGHIJKLMNOPQRSTUVWXYZ "
+var lowercase = " abcdefghijklmnopqrstuvwxyz "
+var numbers = " 0123456789 "
 
 console.log(typeof characters); 
 
@@ -21,10 +24,20 @@ function writePassword() {
 }
 
 function generatePassword() {
-  var click=prompt("How many characters would you like in your password?");
-  if(characters.length < 8){
-    prompt("Password must be more than 8 characters");
+  var generate =prompt("How many characters would you like in your password?");
+  if(generate.length < 8){
+    alert("Password must be more than 8 characters");
   }
+
+  var uppercase = confirm("Would you like uppercase letters in your password?")
+  if(confirm)
+
+  var lowercase = confirm("Would you like lowercase letters in your password?")
+
+  var specialCharacters = confirm("Would you like specialCharacters in your password?")
+
+  var numbers = confirm("Would you like numbers in your password?")
+
 }
 
 // Add event listener to generate button
